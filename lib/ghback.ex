a8428735b45dffdb620ae5bf7b2a736c95dc0279
William Guimont-Martin <wil9876543210@hotmail.com>
2022-08-22 19:16:42 -0400

    basic backup

--- lib/ghback.ex
+++ lib/ghback.ex
@@ -11,7 +11,7 @@ defmodule Ghback do
     backup_path = System.get_env(backup_path_env)
 
     if backup_path == nil do
-      IO.puts("Environment variable #{backup_path_env}} was not set")
+      IO.puts("Environment variable #{backup_path_env} was not set")
       IO.puts("Exiting")
       System.halt()
     end
@@ -22,43 +22,37 @@ defmodule Ghback do
     api = Github.new(token)
 
     {:ok, username} = Github.get_username(api)
-    IO.puts("Will backup repositories into #{backup_path} for user #{username}}")
+    IO.puts("Will backup repositories into #{backup_path} for user #{username}")
 
     list_all_repos(api)
-    |> Stream.map(fn r -> Task.async(fn -> clone_repo(r) end) end)
-    |> Stream.map(&Task.await/1)
+    |> Stream.map(&Task.async(fn -> clone_repo(&1, username, backup_path) end))
+    |> Enum.to_list()
+    |> Enum.map(&Task.await(&1, :infinity))
 
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
   defp list_all_repos(api) do
-    #    list_all_repo_go(api, 1, [], [])
     Stream.transform(
       Stream.iterate(1, &(&1 + 1)),
       1,
-      fn page, acc ->
-        nil
+      fn page, _ ->
+        case Github.list_repositories(api, page) do
+          {:ok, urls} when urls != [] -> {urls, page + 1}
+          _ -> {:halt, page}
+        end
       end
     )
   end
 
-  defp list_all_repo_go(_api, page, [], urls) when page != 1 do
-    urls
-  end
-
-  defp list_all_repo_go(api, page, new_urls, urls) do
-    {:ok, new} = Github.list_repositories(api, page)
-    IO.inspect(length(new))
-    IO.inspect(List.last(new)["name"])
-    list_all_repo_go(api, page + 1, new, new_urls ++ urls)
-  end
-
-  defp clone_repo(repo) do
+  defp clone_repo(repo, username, backup_path) do
     # TODO handle failure
     {:ok, ssh_url} = Map.fetch(repo, "ssh_url")
+    {:ok, name} = Map.fetch(repo, "name")
 
-    if repo["owner"]["login"] == "willGuimont" do
-      IO.puts(ssh_url)
+    if repo["owner"]["login"] == username do
+      "cd #{backup_path} && git clone #{ssh_url}" |> String.to_charlist |> :os.cmd
+      "cd #{backup_path}/#{name} && git pull" |> String.to_charlist |> :os.cmd
     end
   end
 end
